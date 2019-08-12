e1872ea26e57290e310dacaccc61b46e66bbefe4
alekseyVY <alekseyvy@gmail.com>
2019-08-12 22:54:05 +0400

    1

---
+++ build/precache-manifest.cc4bd4ba2f101f16f37a1a16a46ae3d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80f34a583b68f9126d74628d1f9ac634",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c087173f87dfb3f4170",
+    "url": "/static/css/main.20c28737.chunk.css"
+  },
+  {
+    "revision": "edb70b9760340e495c07",
+    "url": "/static/js/2.42c16a5d.chunk.js"
+  },
+  {
+    "revision": "0c087173f87dfb3f4170",
+    "url": "/static/js/main.bf9cb24a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
