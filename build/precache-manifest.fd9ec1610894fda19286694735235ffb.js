14c030b6e6994f930b797286f6758436bc1767f7
alekseyVY <alekseyvy@gmail.com>
2019-08-12 23:03:19 +0400

    uibuild

---
+++ build/precache-manifest.fd9ec1610894fda19286694735235ffb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee4dcec792025b350d4c06ed6c8f5012",
+    "url": "/index.html"
+  },
+  {
+    "revision": "869193d7a2b66cbfa983",
+    "url": "/static/css/main.20c28737.chunk.css"
+  },
+  {
+    "revision": "edb70b9760340e495c07",
+    "url": "/static/js/2.42c16a5d.chunk.js"
+  },
+  {
+    "revision": "869193d7a2b66cbfa983",
+    "url": "/static/js/main.a55a4679.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
