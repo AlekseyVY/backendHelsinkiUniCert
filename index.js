7cc37e1dc1f0e59aef16c63397ef1f38e610dff1
alekseyVY <alekseyvy@gmail.com>
2019-08-20 16:01:22 +0400

    3.15

--- index.js
+++ index.js
@@ -31,13 +31,12 @@ app.get(`/api/persons/:id`, (request, response) => {
     })
 })
 
-/*app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(person => person.id !== id)
-
-    res.send(204)
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id).then(result => {
+        response.status(204).end()
+    }).catch(error => next(error))
 })
-*/
+
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
